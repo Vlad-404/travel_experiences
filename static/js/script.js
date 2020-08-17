2eb242f20684bec785851258c4b650a9f8ca47bb
Vlad-404 <vmijat21@gmail.com>
2020-08-17 15:55:26 +0000

    Added floating action icon

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.slider').slider({indicators: "true"});
     $('.scrollspy').scrollSpy();
+    $('.fixed-action-btn').floatingActionButton();
   });
